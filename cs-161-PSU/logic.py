1a02b185be2300e331fa2c676dca675cf1bdb62b
DarenKostov <daren.kostov@outlook.com>
2023-11-27 13:43:42 -0800

    finished map

--- cs-161-PSU/logic.py
+++ cs-161-PSU/logic.py
@@ -11,38 +11,92 @@ This is part of the Escape Room Game
 
 #WIP game map
 gameMapString=[
-  "########################",
-  "# t>   #               #",
-  "# @j   v #########     #",
-  "#                      #",
-  "##~~~~~#~~~~~~~ #     ##",
-  "# ~~~~~#~~~~~~~ #      #",
-  "# ~~~~~#~~~~~~~ #      #",
-  "# ~~~~~~~~~~~~~ #      #",
-  "# ~~~~~~~~~#~~~ #      #",
-  "#          #    #    ###",
-  "# ######   #      #    #",
-  "#                 #    #",
-  "########################",
+  "##########################################################",
+  "#      J #~~~~~~~~~    i      #    #   #  J             <#",
+  "# @  i   #~~~~~~~~####    $   =  i H<#           ^   <#  #",
+  "#        #~~~~~~~#   #        #    #>       ^   <#>   v  #",
+  "###H######      #  $ #  i     #    #       <#>   v ^     #",
+  "#        =  i +#   $ #        #    #  ^     v     <#>    #",
+  "#  i  ** #######     #>       #    #  #> ^    J    v     #",
+  "##########    ###   ##       <#    #  v <#>     ^        #",
+  "#       #     ~~~~~~~#   ##   #    #     v   ^ <#>  J    #",
+  "#  |J|  #     ~~~~~~~#    v   #    #        <#> v        #",
+  "#  |+|  =     ~~~~~                # J  ^    v           #",
+  "#       #     ~~~~~      ^         #   <#>       ^       #",
+  "####################################    v       <#>      #",
+  "#>        |*        <#      ~~~~~~~~#            v      ##",
+  "#>        |*        <#     #~~~~~~~~ ##      ###      ## #",
+  "#>        |*|*      <#    <#~~~~~~~~ +####         ###   #",
+  "#>                   #>    #~~~~~~~~ +#   ####H####      #",
+  "#>                   #    <#~~~~~~~~ ##      #H#       $ #",
+  "#>                   #>    #~~~~~   #        v v    +    #",
+  "#>                   #    <#~~~~~   #i#              $   #",
+  "#>                   ##   #########H  #             +    #",
+  "#>        |  ***     ## J #        H  #                  #",
+  "#>                   ######         ######################",
+  "#>                   #              #***                <#",
+  "#>           ^^^^^^  #              #**>                 #",
+  "########### ##########              #***                <#",
+  "#==============      #      ##      ###################  #",
+  "#==============#>       #>  $$  <#      <#       *     * #",
+  "#==============      ################         *          #",
+  "#H######### ##########   +       +  ######################",
+  "#      #    #>> >>  v#             #>~~~~~~~~~~~~~~#  i  #",
+  "###  ###v           v#   +    +   #>~~~~~~~~~~~~~~~#     #",
+  "#vv  vv#v    *J*     #           #>~~~~~~~~~~#~~+~~##   ##",
+  "#      #            v#   +  +   #>~~~~~~~~~~~#~~~~~### ###",
+  "#      #>> >> >> >> v#         #>~~~~~~~~~~~~#~~~~~#~~~~~#",
+  "###  ##################  +    #>~~~~~~~~~~~~~#~~~~~#~~~~~#",
+  "#vv    #>            <#      #>~~~~~~~~~~~~~~#~~+~~#~~~~~#",
+  "#                                 #~~~~+~~~~~#~~~~~~~~+~~#",
+  "#>    <#>            <#           #~~~~~~~~~~#~~~~~~~~~~~#",
+  "##########################################################",
 ]
+
+
+'''
+Legend:
+
+# - hard wall, cannot destroy
+| - same as #
+@ - player
+~ - water, can be traversed, but uses up health
+  - air, can be traversed
+> - glider
+< - glider
+v - glider
+^ - glider
+H - door can only be opened with a key
+= - wooden planks
++ - health
+* - fire gem
+$ - gold
+J - key
+i - tells you info, or changes stuff
+
+'''
+
 
 
 gameMap=[]
 
-traversableThings=[" ", "~"]
-breakableThings=["@", "="]
-collectableThings=["$", "J", "*", "+"]
-burnableThings=["=", "+"]
+traversableThings=[' ', '~']
+breakableThings=['@', '=']
+collectableThings=['$', 'J', '*', '+', 'i']
+burnableThings=['=', '+']
 
 
 player=(2, 2)
+playerIsDead=False
+playerWon=False
 visibility=4
 
 inventory={
-  "$": 0,
-  "J": 0,
-  "*": 0,
-  "+": 3
+  '$': 0,
+  'J': 0,
+  '*': 0,
+  '+': 3.0,
+  'i': 0
 }
 
 
@@ -69,13 +123,32 @@ def printBoard():
     print()
 
 
-def isTraversable(thing):
-  global traversableThings
-  
-  for traversable in traversableThings:
-    if thing==traversable:
+def contains(list, chosenElement):
+  for element in list:
+    if element==chosenElement:
       return True
   return False
+
+  
+
+def isTraversable(thing):
+  global traversableThings
+  return contains(traversableThings, thing)
+
+def isBreakable(thing):
+  global breakableThings
+  return contains(breakableThings, thing)
+
+def isCollectable(thing):
+  global collectableThings
+  return contains(collectableThings, thing)
+
+def isBurnable(thing):
+  global burnableThings
+  return contains(burnableThings, thing)
+
+
+
 
 def swap(coord1, coord2):
   temp=gameMap[coord1[0]][coord1[1]]
@@ -88,14 +161,27 @@ def movePlayer(deltaX, deltaY):
 
   global gameMap
   global player
+
+  stuffInfront=gameMap[player[0]+deltaX][player[1]+deltaY]
   
-  if isTraversable(gameMap[player[0]+deltaX][player[1]+deltaY]):
+  if isTraversable(stuffInfront):
+
+    #remove health if water
+    if gameMap[player[0]+deltaX][player[1]+deltaY]=='~':
+      inventory['+']-=0.2;
+
+      #if we loose, we loose
+      if inventory['+']<=0:
+        gameMap[player[0]][player[1]]=" "
+        return
+
     #swap player and the thing we move into
     swap((player[0]+deltaX, player[1]+deltaY),
       (player[0], player[1]))
 
     #move the coordinates of the player
     player=(player[0]+deltaX, player[1]+deltaY)
+  elif isTraversable(stuffInfront):
 
 
 def updateGlider(x, y, dx, dy, altState):
@@ -108,14 +194,14 @@ def updateGlider(x, y, dx, dy, altState):
 def updateGliders():
   for x in range(1, len(gameMap)-1):
     for y in range(1, len(gameMap[0])-1):
-      if gameMap[x][y]==">":
-        updateGlider(x, y, 1, 0, "<")
-      elif gameMap[x][y]=="<":
-        updateGlider(x, y, -1, 0, ">")
-      elif gameMap[x][y]=="^":
-        updateGlider(x, y, 0, -1, "v")
-      elif gameMap[x][y]=="v":
-        updateGlider(x, y, 0, 1, "^")
+      if gameMap[x][y]=='>':
+        updateGlider(x, y, 1, 0, '<')
+      elif gameMap[x][y]=='<':
+        updateGlider(x, y, -1, 0, '>')
+      elif gameMap[x][y]=='^':
+        updateGlider(x, y, 0, -1, 'v')
+      elif gameMap[x][y]=='v':
+        updateGlider(x, y, 0, 1, '^')
         
   
   
@@ -124,11 +210,6 @@ def updateMap():
   updateGliders()
   
 
-
-# printBoard()
-# movePlayer(-1, 0)
-# printBoard()
-# movePlayer(0, -1)
-# printBoard()
+printBoard()
 
 
