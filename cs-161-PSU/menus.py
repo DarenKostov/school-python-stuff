c88a17ed0bd29c028afd6776dbe5c35f500d87f8
DarenKostov <daren.kostov@outlook.com>
2023-11-28 22:45:12 -0800

    removed comments

--- cs-161-PSU/menus.py
+++ cs-161-PSU/menus.py
@@ -30,64 +30,6 @@ def main_menu():
     main_menu()
 
 
-#nice, ill just replace the move function
-#you know what the error is on movePlayer
-#no idea
-#it runs so it should be fine i think ...hmmmm
-#i think it's ok
-# im going to write the invitory menu
-# fixed it!
-
-#btw printBoard() prints the game map
-# where 
-#where to put? after collecting all of the inputs
-#and changing the map
-#aka at the end of each turn
-# im kind confused 
-#about what
-#the map thing 
-#its just where all of the walls, enemies, keys, etc are
-'''
-it looks like this at the moment
-"#" are walls
-"@" is player
-
-"j" and "t" are nothing, just random chars to help me
-make sure that the dirrections are correct
-
-#####
-# j #
-# @t#
-#   #
-#####
-'''
-#okay i understand now thanks bro
-# np
-
-#I have an idea on how to handle the invalid input
-
-#I think we have the foundation
-#player can move and the menu works
-# so like when is the player win like whats the output?
-'''
-======================
-========YOU===========
-==========WIN=========
-======================
-
-something like that ^^^
-
-'''
-# so should i add like if the player collects all of the keys it outpusts that
-#sure, or goes to the final room, or beat the boss
-#I havent actually thought about the main objective
-#The player escapes rooms but what is his goal hmmm :/
-# i think we should give them points or print how many tries they did to win
-#ok, we can have like a validTurns variable that just keeps track of that, yeah
-# for sure
-# should i create new menu about it
-#new function
-
 def main_loop_Menu():
   
   printBoard()
